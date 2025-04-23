037855b41675a9639c0fb3a3d21d2f840d39c18a
Tanishq Sonwane <your-email@example.com>
2025-04-23 12:48:17 +0530

    oops baiscs

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+function student(name, age, number){
+    this.name = name,
+    this.age = age
+    this.number = 20
+}
+ let std1 =  new student('tanishq', 20 )
+ let std2 =  new student('rohit', 40)
+
+console.log(std1, std2)
+
