40e1ac34f82857a4b186133825f7fc7c8b4cea8b
Tanishq Sonwane <your-email@example.com>
2025-04-24 14:22:11 +0530

    Liabrary

--- script.js
+++ script.js
@@ -26,13 +26,51 @@
 
 
 // creating objects using classes
-class Employe{
-    constructor(name,age,salary){
-       this.name = name,
-        this.age = age,
-        this.salary = salary
-    }
+// class Employe{
+//     constructor(name,age,salary){
+//        this.name = name,
+//         this.age = age,
+//         this.salary = salary
+//     }
+// }
+// Employe.prototype.post = 'HR'
+// let t1 = new Employe('tanishq', 20, 52000)
+// let t2 = new Employe('rohit', 19, 48000)
+
+
+
+//  Liabrary in Oops concept
+class Liabrary{
+ constructor(){
+   this.Books = []
+ }
+ addBook(book){
+    this.Books.push(book)
+ }
+ lisAllBooks(){
+  this.Books.forEach((elem, index)=>{
+    console.log(index+1 , '' + `${elem.name} by ${elem.author}`)
+  })
+ }
 }
-Employe.prototype.post = 'HR'
-let t1 = new Employe('tanishq', 20, 52000)
-let t2 = new Employe('rohit', 19, 48000)
+
+class Books{
+  constructor(name, price, author, readStatus){
+  this.name = name,
+  this.price = price
+  this.author = author
+  this.readStatus = false
+  }
+  information(){
+    console.log(`${this.name} is written by ${this.author} and the price of this book is ${this.price} ${this.readStatus? 'You already read this book': 'but you did not read this book'}`)
+  }
+  changeReadStatus(){
+    this.readStatus = !this.readStatus
+    console.log(this.readStatus)
+  }
+}
+let JhansiLiabrary = new Liabrary()
+let book = new Books('Gunaho ka devta', 1200, 'Dharmveer bharti',  )
+
+
+JhansiLiabrary.addBook(book)
